b51411e6460f42b4fba5204f47ef96dcf544b7c1
Matt Burke <burkemm84@gmail.com>
2021-11-11 12:54:13 -0600

    commits

---
+++ new/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Setup from './components/Setup';
+import Display from './components/pages/Portfolio';
+
+
+const App = () => <Setup />;
+<Display/>;
+
+
+export default App;
---
+++ new/src/components/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+
+export default function Home() {
+  return (
+  <div class="flex">
+    <div class="grid-container2">
+        <a href="#" class="button0"></a>
+        Full Stack Software Developer
+        <br></br><br></br>
+        <img src="images/img2.jpg" alt="A picture of Dominque Jenkins" width="400px" height="300px" />      
+        </div>
+  
+  </div>
+    
+  );
+}
