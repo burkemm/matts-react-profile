5f6aa7b11962e4f442188f0e4e53cd5241173c15
Matt Burke <burkemm84@gmail.com>
2021-11-11 13:03:34 -0600

    Delete new directory

--- new/src/components/pages/Resume.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-export default function Resume() {
-    return (
-        <a href="https://drive.google.com/file/d/1itnGgX81HJU8PVZgSw7M6E775U2kg_BX/view?usp=sharing">Resume</a>
-    )
-}
-
